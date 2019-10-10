3df4c09aced4fd5e3140b45fc20b77239070d535
happytree718 <erwinapply2018@163.com>
2019-10-10 11:27:24 -0500

    io w/basicio

--- io.c
+++ io.c
@@ -42,13 +42,13 @@ int main(){
 
   agenda day[7];    //set up an array of struct for each day
   //set up initial value for each struct
-  day[0].day = "Monday";
-  day[1].day = "Tuesday";
-  day[2].day = "Wednesday";
-  day[3].day = "Thursday";
-  day[4].day = "Friday";
-  day[5].day = "Saturday";
-  day[6].day = "Sunday";
+  day[1].day = "Monday";
+  day[2].day = "Tuesday";
+  day[3].day = "Wednesday";
+  day[4].day = "Thursday";
+  day[5].day = "Friday";
+  day[6].day = "Saturday";
+  day[0].day = "Sunday";
 
   int m, n, j, k;
   //initial all the strings to a blank space
@@ -67,6 +67,8 @@ int main(){
   for (m = 0; m < a; m++){
     printf("Name: ");
     scanf("%s", list[m].name);
+    printf("What day? ('0' for Sunday ~ '6' for Sunday)\n");
+    scanf("%d", &list[m].day);
     printf("Start time and end time: ");
     scanf("%d %d", &list[m].start_time, &list[m].end_time);
     list[m].availability = 1;
@@ -79,8 +81,8 @@ int main(){
   slot time[b];
   printf("Please input time slot info below\n");
   for (m = 0; m < b; m++){
-    printf("What day? ('0' for Monday ~ '6' for Sunday)\n");
-    scanf("%d", time[m].day);
+    printf("What day? ('0' for Sunday ~ '6' for Sunday)\n");
+    scanf("%d", &time[m].day);
     printf("Start time and end time: \n");
     scanf("%d %d", &time[m].start_time, &time[m].end_time);
     printf("Number of members per slot: \n");
@@ -88,6 +90,15 @@ int main(){
     time[m].filled = 0;
   }
 
+  for (m = 0; m < a; m++){
+    for (n = 0; n < b; n+){
+      if (list[m].day = time[n].day){
+        if (list[m].start_time > time[n].start_time && list[m].end_time < time[n].end_time){
+          day[list[m].day] = list[m].name;
+        }
+      }
+    }
+  }
   //testing input
   //day[1].time[3] = "James";
   //day[1].time[4] = "James";
@@ -96,6 +107,7 @@ int main(){
   //day[3].time[5] = "Sam";
   //testing part end
 
+  // output table
   for(j = 0; j <= 28; j++){
     //printf("j = %d\n",j);
     line();
